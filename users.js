2bb85dbfc8d77dd5e8d4c23eda9af1b0b85b5208
jck-y <s2200582@student.unklab.ac.id>
2024-04-20 23:47:27 +0800

    first commit

---
+++ users.js
@@ -0,0 +1,28 @@
+const users = [
+  {
+    id: 1,
+    date: "2024-10-27",
+    task_name: "Belajar",
+    task_category: "School",
+    task_description: "Belajar untuk ujian backend",
+    checklist: false,
+  },
+  {
+    id: 2,
+    date: "2024-11-7",
+    task_name: "Tidur",
+    task_category: "Healthy",
+    task_description: "Tidur untuk kesehatan",
+    checklist: false,
+  },
+  {
+    id: 3,
+    date: "2024-10-3",
+    task_name: "Olaharga",
+    task_category: "Healthy",
+    task_description: "olahraga untuk menjaga kesehatan badan",
+    checklist: true,
+  },
+];
+
+module.exports = users;
